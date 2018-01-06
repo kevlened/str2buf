67072d18069661dc95378510ea4c56d397b9f476
kevlened <boyettel@gmail.com>
2018-01-06 13:42:26 -0800

    Add types

--- browser.ts
+++ browser.ts
@@ -1,15 +1,39 @@
+/**
+ * Parse a binary string from a Uint8Array
+ * 
+ * @param {Uint8Array} uint8Array
+ * @returns {String}
+ */
 export function fromUint8Array(uint8Array) {
   return uint8Array.reduce((ag, c) => ag += String.fromCharCode(c), '')
 }
 
+/**
+ * Create a Uint8Array from a binary string
+ * 
+ * @param {String} binaryStr
+ * @returns {Uint8Array}
+ */
 export function toUint8Array(binaryStr) {
   return new Uint8Array(binaryStr.length).map((v, i) => binaryStr.charCodeAt(i))
 }
 
+/**
+ * Create an ArrayBuffer from a binary string
+ * 
+ * @param {ArrayBuffer} buffer
+ * @returns {String}
+ */
 export function fromBuffer(buffer) {
   return fromUint8Array(new Uint8Array(buffer))
 }
 
+/**
+ * Parse a binary string from an ArrayBuffer
+ * 
+ * @param {String} binaryStr
+ * @returns {ArrayBuffer}
+ */
 export function toBuffer(binaryStr) {
   return toUint8Array(binaryStr).buffer
 }
